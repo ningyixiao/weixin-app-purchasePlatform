7c16ed33b6ef7551748e17b5cbf23ade2eaa15e9
ningyixiao <zjutsgg@gmail.com>
2017-03-25 16:52:06 +0800

    13 pages basically completed

--- pages/stockSort/stockSort.js
+++ pages/stockSort/stockSort.js
@@ -1,6 +1,8 @@
 Page({
     data: {
-        customize_position_arr: [],
+        customize_position_arr: [],//每一个二级菜单右侧小箭头的样式数组
+        firstMenu_openState_arr: [],//一级菜单的打开状态数组
+        secondMenu_openState_arr: [],//二级菜单的打开状态数组
         list: [
             {
                 id: '1',
@@ -101,48 +103,87 @@ Page({
 
         ]
     },
-    init_data_arr: function () {
+    init_data_arr: function (all_list) {
+        //初始化全部的二维数组
         var customize_position_arr = [];
-        var temp = [];
-        var list = this.data.list;
-        var length = list.length;
+        var firstMenu_openState_arr = [];
+        var secondMenu_openState_arr = [];
+        var list = all_list;//all_list指api返回的产品列表数据
+        var length = list.length;//列表中一级列表的数量
         for (var i = 0; i < length; i++) {
+            firstMenu_openState_arr[i] = false;//初始化一级菜单的打开状态数组
+            var temp = [];
+            var temp1 = [];
             for (var j = 0; j < list[i].childList.length; j++) {
-                temp[j] = "no_style"
+                temp[j] = "no_style";
+                temp1[j] = false;
             }
-            customize_position_arr[i] = temp;
+            customize_position_arr[i] = temp;//初始化每一个二级菜单右侧小箭头的样式数组
+            secondMenu_openState_arr[i] = temp1;//初始化二级菜单的打开状态数组
         }
         this.setData({
-            customize_position_arr: customize_position_arr
+            customize_position_arr: customize_position_arr,
+            firstMenu_openState_arr: firstMenu_openState_arr,
+            secondMenu_openState_arr: secondMenu_openState_arr
         });
+    },
+    close_other_firstMenu: function (arr, i) {
+        //关闭除了正在打开的其它同级二级菜单
+        var length = arr.length;
+        for (var k = 0; k < length; k++) {
+            k != i && (arr[k] = false);
+        }
+        return arr;
+    },
+    close_other_secondMenu: function (arr, i, j) {
+        //关闭除了正在打开的其它同级二级菜单
+        var length = arr[i].length;
+        for (var k = 0; k < length; k++) {
+            k != j && (arr[i][k] = false);
+        }
+        return arr;
     },
     kindToggle: function (e) {
         var customize_position_arr = this.data.customize_position_arr;
-        var id = e.currentTarget.id, list = this.data.list;
+        var firstMenu_openState_arr = this.data.firstMenu_openState_arr;
+        var secondMenu_openState_arr = this.data.secondMenu_openState_arr;
+        var list = this.data.list;//api返回的产品列表数据
+        var id = e.currentTarget.id;
         for (var i = 0, len = list.length; i < len; ++i) {
             if (list[i].id == id) {
-                list[i].open = !list[i].open
-            } else {
-                for (var j = 0, len = list[i].childList.length; j < len; ++j) {
-                    if (list[i].childList[j].id == id) {
-                        list[i].childList[j].open = !list[i].childList[j].open;
+                firstMenu_openState_arr[i] = !firstMenu_openState_arr[i];
+                //判断一级菜单是否为打开状态,若是,则要将同一级的其它关闭
+                if (firstMenu_openState_arr[i]) {
+                    firstMenu_openState_arr = this.close_other_firstMenu(firstMenu_openState_arr, i);
+                }
+                break;
+            }
+            for (var j = 0, len = list[i].childList.length; j < len; ++j) {
+                if (list[i].childList[j].id == id) {
+                    secondMenu_openState_arr[i][j] = !secondMenu_openState_arr[i][j];
+                    //判断二级菜单是否为打开状态,若是,则要将同一级的其它关闭
+                    if (secondMenu_openState_arr[i][j]) {
+                        secondMenu_openState_arr = this.close_other_secondMenu(secondMenu_openState_arr, i, j);
+                    }
+                    if (customize_position_arr[i][j] == "no_style") {
                         customize_position_arr[i][j] = "customize_position";
+                        console.log(customize_position_arr);
                     } else {
-                        list[i].childList[j].open = false;
                         customize_position_arr[i][j] = "no_style";
                     }
+                    break;
                 }
-                // list[i].open = false
             }
         }
         this.setData({
-            list: list,
-            customize_position_arr: customize_position_arr
+            customize_position_arr: customize_position_arr,
+            firstMenu_openState_arr: firstMenu_openState_arr,
+            secondMenu_openState_arr: secondMenu_openState_arr
         });
     },
     onLoad: function (options) {
         // 页面初始化 options为页面跳转所带来的参数
-        this.init_data_arr();
+        this.init_data_arr(this.data.list);
     },
     onReady: function () {
         // 页面渲染完成
